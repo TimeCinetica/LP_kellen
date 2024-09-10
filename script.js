7cc9813e5c31e6ef0cae2077b094e2b815fb6ee5
Bernardo Krause <107999271+BernardoKrause@users.noreply.github.com>
2024-09-10 20:47:25 -0300

    estilização da seção "o que dizem" completa para versão desktop

--- script.js
+++ script.js
@@ -13,3 +13,29 @@ function removeClassActive () {
         headerLinks[i].classList.remove('active');
     }
 }
+
+// carousel 
+const carousel = document.querySelector('.carousel');
+const carouselInner = document.querySelector('.carousel-inner');
+const carouselItems = document.querySelectorAll('.carousel-item');
+const carouselPrev = document.querySelector('.carousel-prev');
+const carouselNext = document.querySelector('.carousel-next');
+
+let contador = 0;
+
+carouselItems[contador].classList.add('active');
+
+  
+carouselPrev.addEventListener('click', () => {
+    for (let i = 0; i < carouselItems.length; i++) {
+        carouselItems[i].classList.remove('active');
+    }
+    
+});
+  
+carouselNext.addEventListener('click', () => {
+    for (let i = 0; i < carouselItems.length; i++) {
+        carouselItems[i].classList.remove('active');
+    }
+});
+
